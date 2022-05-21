797615c22e4e1652c07360c63e8295882d0eb4ce
Kaiyang Jiang <kpj5148@psu.edu>
2022-05-21 15:43:53 -0400

    added me view

--- GEOApplyApp/View/MeView/MeView.swift
+++ GEOApplyApp/View/MeView/MeView.swift
@@ -1,19 +1,47 @@
 //
-//  MeView.swift
-//  GEOApplyApp
+//  ContentView.swift
+//  Me View
 //
-//  Created by Matthew Jiang on 2022/4/28.
+//  Created by Chris Yang on 5/12/22.
 //
 
 import SwiftUI
 
 struct MeView: View {
+    @EnvironmentObject var manager : AppManager
     var body: some View {
-        ScrollView{
-            Text("Me View")
+        NavigationView {
+            ScrollView {
+                VStack {
+                    Spacer().frame(height: 100)
+                    BasicCard(userName: manager.currentUser?.name ?? "unknown", likeNumber: 100, viewNumber: 45, postNumber: 10)
+                        .padding()
+                    Spacer().frame(height: 10)
+                    TopTab()
+                    Button(action: {
+                        manager.signOut()
+                    }){
+                        Text("Sign Out").foregroundColor(.white).font(.title2).padding(EdgeInsets(top: 10, leading: manager.screenWidth*0.33, bottom: 10, trailing: manager.screenWidth*0.33)).background(RoundedRectangle(cornerRadius: 10).fill(.red.opacity(0.8)))
+                    }.padding(5)
+                }
+                .padding()
+                .navigationTitle("Personal Center")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(destination: SettingView()){
+                            Image(systemName: "gearshape")
+                        }
+                    }
+                        }
+            }
         }
-        
+     
     }
 }
 
-
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
